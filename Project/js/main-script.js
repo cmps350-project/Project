78239a557e03641e7b883996cc86a4f547a957f3
fatma-fa2002307 <fa2002307@qu.edu.qa>
2024-03-25 22:57:00 +0300

    db

--- Project/js/main-script.js
+++ Project/js/main-script.js
@@ -1,9 +1,1 @@
-import artworks from "../db/artworkData"; // Import the artwork data
-
-function saveArtworksToLocalStorage() {
-  localStorage.setItem('artworks', JSON.stringify(artworks));
-}
-
-//saveArtworksToLocalStorage();
-
-console.log(artworks);
+console.log("ok")
