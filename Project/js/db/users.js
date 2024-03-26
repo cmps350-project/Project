6ef105390b161b048d402001ceb3017cb42ecd48
Shatha-SA2108114 <sa2108114@qu.edu.qa>
2024-03-27 02:49:58 +0300

    Update users.js

--- Project/js/db/users.js
+++ Project/js/db/users.js
@@ -1,49 +1,70 @@
 const users = [
     {
-      username: "user1",
+      username: "ahmed_omar",
       password: "password1",
+      fullName: "Ahmed Omar",
       email: "user1@example.com",
+      type: "seller",
       currentBasket: [1, 3],
       purchaseHistory: [],
       currentSelling: [],
       soldHistory: []
     },
     {
-      username: "user2",
+      username: "sara_ali",
       password: "password2",
       email: "user2@example.com",
+      fullName: "Sara Ali",
+      type: "customer",
       currentBasket: [2, 4],
       purchaseHistory: [],
       currentSelling: [],
       soldHistory: []
     },
     {
-      username: "user3",
+      username: "john_smith",
       password: "password3",
       email: "user3@example.com",
+      fullName: "John Smith",
+      type: "seller",
       currentBasket: [],
       purchaseHistory: [],
       currentSelling: [],
       soldHistory: []
     },
     {
-      username: "user4",
+      username: "jane_brown",
       password: "password4",
       email: "user4@example.com",
+      fullName: "Jane Brown",
+      type: "customer",
       currentBasket: [],
       purchaseHistory: [],
       currentSelling: [],
       soldHistory: []
     },
     {
-      username: "user5",
+      username: "maryam_amer",
       password: "password5",
       email: "user5@example.com",
+      fullName: "Maryam Amer",
+      type: "admin",
       currentBasket: [],
       purchaseHistory: [],
       currentSelling: [],
       soldHistory: []
-    }
+    },
+    {
+        username: "saif_osama",
+        password: "password6",
+        email: "user6@example.com",
+        fullName: "Saif Osama",
+        type: "admin",
+        currentBasket: [],
+        purchaseHistory: [],
+        currentSelling: [],
+        soldHistory: []
+      }
   ]
 
  export function getUsers(){
