a4f893b1f5b2672f696b41d29fe43c41fa2f91b7
fatma-fa2002307 <fa2002307@qu.edu.qa>
2024-05-09 21:25:42 +0300

    Update NavBar.jsx

--- project-phase2 copy/app/common/NavBar.jsx
+++ project-phase2 copy/app/common/NavBar.jsx
@@ -1,9 +1,29 @@
+'use client'
 import React from 'react'
 import styles from '@/app/styles/page.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
 
 
 export default function NavBar() {
+    const router = useRouter();
+    
+      const handleSellYourArtClick = async() => {
+        const userId = localStorage.getItem('userId');
+        if (userId) {
+            const response = await fetch(`/api/users/sellers/${userId}`)
+            const user = await response.json(); 
+            if (user) {
+              router.push(`/artworks/${userId}/upsert`);
+            }else{
+                alert("You are not a seller and therefore cannot access this page");
+            }
+        }else{
+          alert('You are not logged in, please login as seller to sell your art');
+          router.push(`/users/login`);
+        }
+      };
+
   return (
     <>
     <div className={styles.pageHead}>
@@ -12,7 +32,7 @@ export default function NavBar() {
                 <ul className = {styles.menu}>
                     <li className = {styles.menuItem}><Link href="/">Home</Link></li>
                     <li className = {styles.menuItem} id = "acc-page"><a>View Account</a></li>
-                    <li className = {styles.menuItem} id = "sell-page"><a>Sell Your Art</a></li>                       
+                    <li className = {styles.menuItem} id = "sell-page" onClick={handleSellYourArtClick}>Sell Your Art</li>                       
                     <li className = {styles.menuItem}><Link href="#ethical-commitment">About Us</Link></li>
                 </ul>
             </nav>
