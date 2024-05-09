8c9c7d33b08e0870a4cb1fb4156228abf2e0e0a6
fatma-fa2002307 <fa2002307@qu.edu.qa>
2024-05-09 19:46:04 +0300

    d
    d

--- project-phase2 copy/app/api/users/customers/route.js
+++
@@ -1,6 +0,0 @@
-import userRepo from "@/app/repo/user-repo";
-
-export async function GET(request) {
-    const users = await userRepo.getAllCustomers();
-    return Response.json(users, { status: 200 })
-}
