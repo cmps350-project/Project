c9b82e2a9175e794dc5a8eaf14b1dcdd2e3d3601
Shatha-SA2108114 <sa2108114@qu.edu.qa>
2024-05-10 16:45:27 +0300

    up

--- project-phase2 copy/app/artworks/[userId]/viewSeller/page.jsx
+++ project-phase2 copy/app/artworks/[userId]/viewSeller/page.jsx
@@ -1,28 +1,7 @@
-// import React from 'react'
-// import User from '@/app/artworks/[userId]/User'
-// import userRepo from '@/app/repo/user-repo'
-// import styles from '@/app/styles/page.module.css'
-// import artworkRepo from '@/app/repo/artwork-repo'
-
-
-// export default async function page({params}) {
-//   const userId = params.userId
-//   const user = await userRepo.getSellerByUserId(userId)
-
-//   const sellerArtworks = await artworkRepo.getArtworksBySellerId(userId);
-
-//   return (
-//     <div className={styles.pageBody}>
-//         <User user={user}></User>
-//         </div>
-    
-//   )
-
-// }
 import React from 'react';
 import User from '@/app/artworks/[userId]/User';
 import userRepo from '@/app/repo/user-repo';
-import artworkRepo from '@/app/repo/artwork-repo'; // Assuming you have renamed your repository file to artwork-repo
+import artworkRepo from '@/app/repo/artwork-repo'; 
 import styles from '@/app/styles/page.module.css';
 
 export default async function SellerPage({ params }) {
